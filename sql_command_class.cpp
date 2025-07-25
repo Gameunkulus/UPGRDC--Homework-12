146e0f3a4784d697091d9670c552f1c5fdc47d93
Gameunkulus <bugr24-8@mail.ru>
2025-07-25 21:54:01 +0300

    program redacted

---
+++ sql_command_class.cpp
@@ -0,0 +1,152 @@
+﻿#include "sql_command_class.h"
+
+#using namespace std;
+
+DataBase::DataBase(const string connection) : connection_{ connection } { cout << "Connected to database successfully!" << endl; }
+
+void DataBase::createDatabaseTables()
+{
+	try
+	{
+		pqxx::work txn(connection_);
+		txn.exec(R"(CREATE TABLE IF NOT EXISTS clients(id SERIAL PRIMARY KEY, name TEXT, surname TEXT, email TEXT UNIQUE);
+		CREATE TABLE IF NOT EXISTS phones(id SERIAL PRIMARY KEY, client_id INT REFERENCES clients(id), phone TEXT UNIQUE);)");
+		txn.commit();
+		cout << "A database structure (tables) created" << endl;
+	}
+	catch (const exception& ex)
+	{
+		SetConsoleCP(CP_UTF8);
+		SetConsoleOutputCP(CP_UTF8);
+		cerr << "Exception happened: " << ex.what() << endl;
+	}
+
+}
+
+void DataBase::addClient(const string& name, const string& surname, const string& email, const string& phone) 
+{
+	try
+	{
+		pqxx::work txn(connection_);
+		connection_.set_client_encoding("UTF8");
+		pqxx::result res = txn.exec_params("INSERT INTO clients (name, surname, email) VALUES ($1, $2, $3) REURNING id", name, surname, email);
+		if (!res.empty()) {
+			int client_id = res[0][0].as<int>();
+			txn.exec_params("INSERT INTO phones (client_id, phone) VALUES ($1, $2)", client_id, phone);
+			txn.commit();
+			cout << "Client added with ID: " << client_id << endl;
+		}
+		else {
+			txn.abort();
+			cerr << "Failed to add client" << endl;
+		}
+		
+	}
+	catch (const exception& ex)
+	{
+		SetConsoleCP(CP_UTF8);
+		SetConsoleOutputCP(CP_UTF8);
+		cerr << "Exception happened: " << ex.what() << endl;
+	}
+}
+
+void DataBase::addPhone(const std::string& name, const std::string& phone) // Adding a Phone for an Existing Customer
+{
+	pqxx::work txn(connection_);
+	pqxx::result res = txn.exec_params("SELECT id, surname FROM clients WHERE name = $1", name);
+	if (!res.empty())
+	{
+		int client_id = res[0][0].as<int>();
+		txn.exec_params("INSERT INTO phones (client_id, phone) VALUES ($1, $2)", client_id, phone);
+
+		std::string client_surname = res[0][1].as<std::string>();
+		txn.commit();
+		std::cout << "Phone added for client: " << client_id << " " << name << " " << client_surname << std::endl;
+	}
+	else
+	{
+		txn.abort();
+		std::cerr << "Client not found: " << name << std::endl;
+	}
+}
+
+void DataBase::updateClient(const std::string& email, const std::string& newName, const std::string& newSurname, const std::string& newEmail) //Changing customer data
+{
+	pqxx::work txn(connection_);
+	pqxx::result res = txn.exec_params("SELECT id FROM clients WHERE email = $1", email);
+	if (!res.empty())
+	{
+		int client_id = res[0][0].as<int>();
+		txn.exec_params(R"(UPDATE clients SET name = $1, surname = $2, email = $3 WHERE id = $4)", newName, newSurname, newEmail, client_id);
+		txn.commit();
+		std::cout << "Client updated: " << client_id << " " << newName << " " << newSurname << newEmail << std::endl;
+	}
+	else
+	{
+		txn.abort();
+		std::cerr << "Client not found: " << newName << std::endl;
+	}
+}
+
+void DataBase::removePhone(const std::string& email, const std::string& phone) //Deleting a phone for an existing client
+{
+	pqxx::work txn(connection_);
+	pqxx::result res = txn.exec_params("SELECT id, name, surname FROM clients WHERE email = $1", email);
+	if (!res.empty())
+	{
+		int client_id = res[0][0].as<int>();
+		txn.exec_params("DELETE FROM phones WHERE client_id = $1 AND phone = $2", client_id, phone);
+		txn.commit();
+		std::string client_name = res[0][1].as<std::string>();
+		std::string client_surname = res[0][2].as<std::string>();
+		std::cout << "Phone removed for client: " << client_id << " " << client_name << " " << client_surname << std::endl;
+	}
+	else
+	{
+		txn.abort();
+		std::cerr << "Client not found: " << email << std::endl;
+	}
+}
+
+void DataBase::removeClient(const std::string& email) //Deleting an existing client
+{
+	pqxx::work txn(connection_);
+	pqxx::result res = txn.exec_params("SELECT id, name, surname FROM clients WHERE email = $1", email);
+	if (!res.empty()) {
+		int client_id = res[0][0].as<int>();
+		std::string client_name = res[0][1].as<std::string>();
+		std::string client_surname = res[0][2].as<std::string>();
+		txn.exec_params("DELETE FROM phones WHERE client_id = $1", client_id);
+		txn.exec_params("DELETE FROM clients WHERE id = $1", client_id);
+		txn.commit();
+		std::cout << "Client removed: " << client_id << " " << client_name << " " << client_surname << std::endl;
+	}
+	else {
+		txn.abort();
+		std::cerr << "Client not found: " << email << std::endl;
+	}
+}
+
+
+std::vector<Client> DataBase::findClients(const std::string& query)
+{
+	std::vector<Client> result;
+	pqxx::work txn(connection_);
+	pqxx::result res = txn.exec_params(
+		"SELECT * "
+		"FROM clients "
+		"LEFT JOIN public.phones on public.clients.id = public.phones.client_id "
+		"WHERE clients.name = $1 OR clients.surname = $1 OR clients.email = $1 OR phones.phone = $1;", query);
+	txn.commit();
+	for (auto row : res) {
+		Client client;
+		client.name_ = row["name"].as<std::string>();
+		client.surname_ = row["surname"].as<std::string>();
+		client.email_ = row["email"].as<std::string>();
+		result.push_back(client);
+	}
+	return result;
+}
+
+
+
---
+++ sql_command_class.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <pqxx/pqxx>
+#include <Windows.h>
+#include <string>
+#include <codecvt>
+
+using namespace std;
+
+struct Client
+{
+	string name;
+	string surname;
+	string email;
+	vector<string> phones;
+};
+
+class DataBase
+{
+private:
+	pqxx::connection link;
+
+	void createDataTables();
+	void addClient(const string& name, const string& surname, const string& email, const string& phone);
+	void addPhone(const string& name, const string& phone);
+	void updateClient(const string& email, string& newName, const string& newSurname, const string& newEmail);
+	void removePhone(const string& email, const string& phone);
+	void removeClient(const string& email);
+
+	vector<Client> findClients(const string& query);
+
+	void show();
+};
